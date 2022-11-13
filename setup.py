0aff66546f95c05921ebd5ac0301253a647df930
Andri Fachrur Rozie <4r53n1c@gmail.com>
2022-11-13 23:36:44 +0700

    Add Requirements

--- setup.py
+++ setup.py
@@ -15,14 +15,18 @@ setup(name='bangkits',
       packages=find_packages(),
       package_data={'bangkits': ['bangkits/data/*.*']},
       include_package_data = True,
-      install_requires=[
-          'numpy==1.13.3',
-          'pandas==0.21.0',
-          'python-dateutil==2.6.1',
-          'pytz==2017.3',
-          'scikit-learn==0.19.0',
-          'scipy==1.0.0',
-          'six==1.11.0',
-          'sklearn==0.0'
+      install_requires=[          
+        'keras==2.10.0',
+        'Keras-Preprocessing==1.1.2',        
+        'matplotlib==3.5.3',        
+        'nltk==3.7',
+        'numpy==1.21.6',        
+        'pandas==1.1.5',        
+        'requests==2.28.1',        
+        'scikit-learn==1.0.2',
+        'scipy==1.7.3',                
+        'tensorflow==2.10.0',        
+        'urllib3==1.26.12',
+        'xlrd==2.0.1'        
       ],
       zip_safe=False)
